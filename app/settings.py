7063870ebc6f0c2e4468c7f37cf51d5b1ab7018f
swer <you@youraddress.com>
2022-07-20 23:52:16 +0300

    tewat app heroku

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,3 +117,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+django_heroku.settings(locals())
